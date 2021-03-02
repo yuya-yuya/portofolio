fae63956dcd900edd1e3158a1d7936d3bce50abd
yuya-yuya <yytyfp5115@icloud.com>
2021-03-02 13:54:25 +0900

    initial commit

---
+++ portfolio1/main.js
@@ -0,0 +1,21 @@
+(function(){
+  $('#case1js').on('click', function() {
+    $('#case1desc').toggleClass('case1des');
+  });
+
+  $('#case2js').on('click', function() {
+    $('.case2des').toggle('slow');
+  });
+
+  $('#case3js').on('click', function() {
+    $('.case3des').toggle('slow');
+  });
+
+  $('#case4js').on('click', function() {
+    $('.case4des').toggle('slow');
+  });
+
+  $('#case5js').on('click', function() {
+    $('.case5des').toggle('slow');
+  });
+});
